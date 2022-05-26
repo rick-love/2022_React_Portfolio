c539bc68529b20af2f18ccfaebf9dbc2681f94c5
rick-love <ricklove@outlook.com>
2022-05-26 22:14:47 +0200

    update

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>Hi, It's me</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
