0a1a1ee9eb9528cf11fd64e4a7d46ae916890b69
rick-love <ricklove@outlook.com>
2022-05-27 22:56:44 +0200

    fixing Fontawesome issue

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,8 +4,7 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-// import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 // import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
