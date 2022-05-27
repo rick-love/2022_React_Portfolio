932c00eefd5ed6a0a4ff863c5b1e109cedb8e265
rick-love <ricklove@outlook.com>
2022-05-27 22:08:13 +0200

    2

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,9 +3,9 @@ import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "~@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "~@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "~@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
